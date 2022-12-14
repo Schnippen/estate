1da9cc8833f31112df141180c8c32f815b88a4e2
Schnippen <68897609+Schnippen@users.noreply.github.com>
2022-12-14 19:22:54 +0100

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-//import {Route,Routes} from "react-router-dom"
 import LandingPage from "./pages/LandingPage";
 import Item from "./pages/Item";
 import Offers from "./pages/Offers";
 import NotFound from "./components/NotFound";
+//import SignUp from "./components/SignUp";
 import { Routes, Route } from "react-router-dom";
+import { lazy } from "react";
 
 /* <Route path='/' element={<Home/>}/> */
+
+const SignUp = lazy(()=> import("./components/SignUp"));
 
 function App() {
   return (
@@ -14,6 +17,7 @@ function App() {
         <Route path="/" element={<LandingPage />} />
         <Route path="/Offers" element={<Offers />} />
         <Route path="/Item" element={<Item />} />
+        <Route path="/SignUp" element={<SignUp />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </>
--- src/components/UserInterface.jsx
+++ src/components/UserInterface.jsx
@@ -4,35 +4,45 @@ import {
   HiHeart,
   HiCog,
   HiOfficeBuilding,
+  HiUserCircle,
 } from "react-icons/hi";
 import styles from "./UserInterface.module.css";
 import useActive from "./useActive";
 import { useRef, useEffect } from "react";
+import { Link } from "react-router-dom";
 
 function UserInterface() {
   const [isActiveBig, setIsActiveBig] = useActive(false);
-  const [isActive, setIsActive] = useActive(false);
+  const [isActiveSmall, setIsActive] = useActive(false);
 
   const ref = useRef();
 
   useEffect(() => {
     const handleClose = (e) => {
-      if (isActive && ref.current && !ref.current?.contains(e.target)) {
+      if (isActiveSmall && ref.current && !ref.current?.contains(e.target)) {
         setIsActive();
+      } else if (
+        isActiveBig &&
+        ref.current &&
+        !ref.current?.contains(e.target)
+      ) {
+        setIsActiveBig();
       }
     };
     document.addEventListener("click", handleClose);
     return () => {
       document.removeEventListener("click", handleClose);
     };
-  }, [isActive]);
+  }, [isActiveSmall, isActiveBig]);
 
-  const openBtn1Big =()=>{
-    if(isActive){
+  const openBtn1Big = () => {
+    if (isActiveSmall) {
       setIsActive();
       setIsActiveBig();
-    }else{setIsActiveBig();}
-  }
+    } else {
+      setIsActiveBig();
+    }
+  };
   const openBtn2 = () => {
     if (isActiveBig) {
       setIsActiveBig();
@@ -42,17 +52,24 @@ function UserInterface() {
     }
   };
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    alert("Loging In!");
+  };
+
   return (
     <div className={styles.btn__group} ref={ref}>
       <button
         type="button"
         className={
           //isActive
-          //  ? `${styles.btn} ${styles.btn1_btn2_Opened}`
-          //  : `${styles.btn} ${styles.btn1}`
+          //  ? `${styles.btn} ${styles.btnBig_btnSmall_Opened}`
+          //  : `${styles.btn} ${styles.btnBig}`
           isActiveBig
-            ? `${styles.btn} ${styles.btn1_btn2_Opened}  `
-            : `${styles.btn} ${styles.btn1}`
+            ? `${styles.btn} ${styles.btnBig_Opened} `
+            : isActiveSmall
+            ? `${styles.btn} ${styles.btnBig_btnSmall_Opened}`
+            : `${styles.btn} ${styles.btnBig}`
         }
         onClick={openBtn1Big}
       >
@@ -61,22 +78,67 @@ function UserInterface() {
       <button
         type="button"
         className={
-          isActive
-            ? `${styles.btn} ${styles.btn2_Opened}`
-            : `${styles.btn} ${styles.btn2}`
+          isActiveSmall
+            ? `${styles.btn} ${styles.btnSmall_Opened}`
+            : isActiveBig
+            ? `${styles.btn} ${styles.btnBig_Opened_btnSmall}`
+            : `${styles.btn} ${styles.btnSmall}`
         }
         onClick={openBtn2}
       >
         <HiDotsHorizontal />
       </button>
-      <div className={isActiveBig ? styles.modal_btn1_Opened : null}></div>
+      <div className={isActiveBig ? styles.menu_btnBig_Opened : null}>
+        {isActiveBig ? (
+          <form onSubmit={handleSubmit} className={styles.form}>
+            <div className={styles.form_div_wrapper}>
+              <label for="email" className={styles.form_div_label}>
+                E-mail
+              </label>
+              <input
+                id="email"
+                name="email"
+                type="email"
+                placeholder="E-mail"
+                className={styles.inputText}
+                onChange={(e) => console.log(e.target.value)}
+                required
+              />
+            </div>
+            <div className={styles.form_div_wrapper}>
+              <label for="password" className={styles.form_div_label}>
+                Password
+              </label>
+              <input
+                id="password"
+                name="password"
+                type="password"
+                placeholder="Password"
+                className={styles.inputText}
+                onChange={(e) => console.log(e.target.value)}
+                required
+              />
+            </div>
+
+            <input value="Sign In" type="submit" className={styles.submit} />
+
+            <p>Forgot your password?</p>
+            <div className={styles.form_div_actions}>
+              <p>Dont have Account?</p>
+              <Link to="/SignUp">
+                <p>Sign Up!</p>
+              </Link>
+            </div>
+          </form>
+        ) : null}
+      </div>
       <div
-        className={isActive ? styles.modal_btn2_Opened : null}
+        className={isActiveSmall ? styles.menu_btnSmall_Opened : null}
         onClick={(e) => console.log(e)}
         //Modal dla button 2
       >
-        {isActive ? (
-          <ul className={styles.modal_list}>
+        {isActiveSmall ? (
+          <ul className={styles.menu_list}>
             <li>
               <HiOfficeBuilding />
               Ogłoszenia{" "}
